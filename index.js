2edd342bf3f02a0926a1a678a81a47411b3bb3e9
sdjhen <114769817+sdjhen@users.noreply.github.com>
2023-02-27 23:57:06 +0000

    added create manager function & pushed to team array

--- index.js
+++ index.js
@@ -1,15 +1,58 @@
-const Manager = require("./lib/Manager");
-const Engineer = require("./lib/Engineer");
-const Intern = require("./lib/Intern");
-const inquirer = require("inquirer");
-const path = require("path");
-const fs = require("fs");
+#!/usr/bin/env nodec
 
-const OUTPUT_DIR = path.resolve(__dirname, "output");
-const outputPath = path.join(OUTPUT_DIR, "team.html");
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+const inquirer = require('inquirer');
+const path = require('path');
+const fs = require('fs');
 
-const render = require("./src/page-template.js");
+const OUTPUT_DIR = path.resolve(__dirname, 'output');
+const outputPath = path.join(OUTPUT_DIR, 'team.html');
 
+const render = require('./src/page-template.js');
+const teamArr = [];
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
 
+function createManager(team) {
+  inquirer
+    .prompt([
+      // Manager name
+      {
+        type: 'input',
+        name: 'name',
+        message: "What is the team manager's name?",
+      },
+      // Manager id
+      {
+        type: 'input',
+        name: 'id',
+        message: "What are the manager's ID credentials?",
+      },
+      // Manager email
+      {
+        type: 'input',
+        name: 'email',
+        message: "What is the manager's Email?",
+      },
+      // Manager office number (phone number)
+      {
+        type: 'input',
+        name: 'officeNumber',
+        message: "What is the Manager's Office Number?",
+      },
+    ])
+    .then((managerDetails) => {
+      // Init Manager class to create Manager object
+      const manager = new Manager(
+        managerDetails.name,
+        managerDetails.id,
+        managerDetails.email,
+        managerDetails.officeNumber
+      );
+      teamArr.push(manager);
+      console.log(manager);
+    });
+}
+createManager();
